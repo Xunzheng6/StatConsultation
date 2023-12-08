0a479789db617d847c407e5cb47f913d99963d05
batata38 <52805255+batata38@users.noreply.github.com>
2023-12-08 16:29:07 -0500

    m9

--- merge_analysis.R
+++ merge_analysis.R
@@ -3,15 +3,35 @@ library(lubridate)
 library(zoo)
 
 # appending the data
-merged_all <- rbind(Movie2021H1, Movie2021H2, Movie2022H1, Movie2022H2, Movie2023H1, Movie2023H2)
+Movie2018H1 <- load("Movie2018H1.Rdata")
+Movie2018H1 <- MovieFile
+Movie2018H2 <- load("Movie2018H2.Rdata")
+Movie2018H2 <- MovieFile
+
+Movie2019H1 <- load("Movie2019H1.Rdata")
+Movie2019H1 <- MovieFile
+Movie2019H2 <- load("Movie2019H2.Rdata")
+Movie2019H2 <- MovieFile
+
+Movie2020H1 <- load("Movie2020H1.Rdata")
+Movie2020H1 <- MovieFile
+Movie2020H2 <- load("Movie2020H2.Rdata")
+Movie2020H2 <- MovieFile
+
+merged_all <- rbind(Movie2018H1, Movie2018H2, 
+                    Movie2019H1, Movie2019H2,
+                    Movie2020H1, Movie2020H2,
+                    Movie2021H1, Movie2021H2, 
+                    Movie2022H1, Movie2022H2, 
+                    Movie2023H1, Movie2023H2)
 
 # keeping only movies using the /movies/ pattern in response.docs.web_url
 merged <- merged_all %>%
   filter(str_detect(response.docs.web_url, "/movies"))
-# we have 2229 movies, now keep only unique ones
+# we have 4268 movies, now keep only unique ones
 #merged_unique <- unique(merged)
 merged_unique <- merged %>% distinct(response.docs.snippet, .keep_all = TRUE)
-#only 2103 unique!
+#only 4142 unique!
 
 # to know what type of movie, we can find this information in the response.docs.keywords list
 typelist <- merged_unique$response.docs.keywords
@@ -66,9 +86,40 @@ trend1 %>%
   theme_minimal() 
 
 # trend number of articles over time
+covid2020p1 <- load("Covid2020P1.Rdata")
+covid2020p1 <- CovidFile
+covid2020p2 <- load("Covid2020P2.Rdata")
+covid2020p2 <- CovidFile
+covid2020p3 <- load("Covid2020P3.Rdata")
+covid2020p3 <- CovidFile
+covid2020p4 <- load("Covid2020P4.Rdata")
+covid2020p4 <- CovidFile
+covid2020p5 <- load("Covid2020P5.Rdata")
+covid2020p5 <- CovidFile
+covid2020p6 <- load("Covid2020P6.Rdata")
+covid2020p6 <- CovidFile
+covid2020p7 <- load("Covid2020P7.Rdata")
+covid2020p7 <- CovidFile
+covid2020p7 <- covid2020p7 %>% select(-response.docs.slideshow_credits)
+covid2020p8 <- load("Covid2020P8.Rdata")
+covid2020p8 <- CovidFile
+covid2020p8 <- covid2020p8 %>% select(-response.docs.slideshow_credits)
 
+covid <- rbind(covid2020p1,covid2020p2,
+               covid2020p3,covid2020p4,
+               covid2020p5,covid2020p6,
+               covid2020p7,covid2020p8) #13114
+covid_unique <- covid %>% distinct(response.docs.snippet, .keep_all = TRUE) #10415 unique
 
+trend2 <- covid_unique %>% mutate(month = month(response.docs.pub_date), 
+                                  year = year(response.docs.pub_date))
+covid_unique$monthyear <- as.yearmon(paste(trend2$year, trend2$month), "%Y %m")
 
+# trend covid articles
+unique <- covid_unique %>% select(response.docs.pub_date,month, year, monthyear, response.docs.news_desk, type, author, criticpick)
+trend2 <- unique %>% group_by(monthyear) %>% mutate(n_month=n()) %>% ungroup() %>% group_by(year) %>% 
+  mutate(n_year=n()) %>% ungroup() %>% group_by(monthyear) %>% arrange(monthyear)
+trend2$monthyear_c <- as.character(trend2$monthyear)
 
 
 
