f1780d11b8dc8a0e2532bd9693c27ad70fb2f455
batata38 <52805255+batata38@users.noreply.github.com>
2023-12-08 12:38:42 -0500

    m7

--- merge_analysis.R
+++ merge_analysis.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(lubridate)
+library(zoo)
 
 # appending the data
 merged_all <- rbind(Movie2021H1, Movie2021H2, Movie2022H1, Movie2022H2, Movie2023H1, Movie2023H2)
@@ -25,9 +27,50 @@ check <- merged_unique %>% filter (merged_unique$type!="Movies" & merged_unique$
 merged_unique <- merged_unique %>% mutate(type=0)
 for (i in 1:nrow(merged_unique)) {
   typetable=as.data.frame(merged_unique$response.docs.keywords[i])
-  merged_unique$type[i]=ifelse(any(typetable$value=="Documentary Films and Programs"),"Documentary Films and Programs",ifelse(any(typetable$value=="Movies"),"Movies",NA))
+  merged_unique$type[i]=ifelse(any(typetable$value=="Documentary Films and Programs"),"Documentary Films and Programs",
+                               ifelse(any(typetable$value=="Movies"),"Movies",
+                                      ifelse(any(typetable$value=="Animated Films"),"Animated Films",NA)))
 }
 check2 <- merged_unique %>% filter (is.na(merged_unique$type))
+
+# Add author name (removing "By")
+stopwords = c("By")
+merged_unique$author <- merged_unique$response.docs.byline.original<-gsub("By","",as.character(merged_unique$response.docs.byline.original))
+
+# clean critic's pick column
+merged_unique$criticpick <- ifelse(is.na(merged_unique$response.docs.headline.kicker),0,1)
+table(merged_unique$criticpick)
+
+# clean date column
+merged_unique <- merged_unique %>%
+  mutate (month = month(response.docs.pub_date), 
+          year = year(response.docs.pub_date))
+merged_unique$monthyear <- as.yearmon(paste(merged_unique$year, merged_unique$month), "%Y %m")
+
+# trend number of movie reviews
+unique <- merged_unique %>% select(response.docs.pub_date,month, year, monthyear, response.docs.news_desk, type, author, criticpick)
+trend1 <- unique %>% group_by(monthyear) %>% mutate(n_month=n()) %>% ungroup() %>% group_by(year) %>% 
+  mutate(n_year=n()) %>% ungroup() %>% group_by(monthyear) %>% arrange(monthyear)
+trend1$monthyear_c <- as.character(trend1$monthyear)
+
+esquisse::esquisser()
+
+trend1 %>%
+  arrange(monthyear) %>% 
+  group_by(monthyear, year) %>% 
+  summarise(n_month=n()) %>% 
+  ggplot() +
+  aes(x = monthyear, y = n_month) +
+  geom_line()+
+  scale_color_gradient() +
+  theme_minimal() 
+
+# trend number of articles over time
+
+
+
+
+
 
 
 
