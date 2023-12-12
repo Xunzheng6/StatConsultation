c0bf03dcc6cbb14243177a6e235f06de8ccd58c9
Xunzheng6 <xunqing.zheng@gmail.com>
2023-12-12 17:17:36 -0500

    update

---
+++ imbddata.R
@@ -0,0 +1,18 @@
+
+
+imdbTSVfiles <- function(fileName){
+  url <- paste0("https://datasets.imdbws.com/",fileName,".tsv.gz")
+  tmp <- tempfile()
+  download.file(url, tmp)
+  
+  assign(fileName,
+         readr::read_tsv(
+           file = gzfile(tmp),
+           col_names = TRUE,
+           quote = "",
+           na = "\\N"),
+         envir = .GlobalEnv)
+}
+
+imdbTSVfiles("title.basics")
+imdbTSVfiles("title.ratings")
