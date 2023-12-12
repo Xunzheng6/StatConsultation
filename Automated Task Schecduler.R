efcb9b7079a95e933e64dfb2c95913f273df0a07
Xunzheng6 <xunqing.zheng@gmail.com>
2023-12-12 17:20:42 -0500

    update

--- Automated Task Schecduler.R
+++ Automated Task Schecduler.R
@@ -9,5 +9,11 @@ getwd()
 cmd = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/CovidAPI.R")
 cron_add(cmd, "daily", at="5PM")
 
+cmd1 = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/MovieAPI.R")
+cron_add(cmd1, "daily", at="4PM")
+
+cmd2 = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/imbddata.R")
+cron_add(cmd1, "daily", at="3PM")
+
 ##Window
 taskschedulerAddin()
