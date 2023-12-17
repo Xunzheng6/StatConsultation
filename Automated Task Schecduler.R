02051e7b64c0d9a2b5ef68a5cf2a0303f48da110
Xunzheng6 <xunqing.zheng@gmail.com>
2023-12-17 10:04:28 -0500

    Update

--- Automated Task Schecduler.R
+++ Automated Task Schecduler.R
@@ -1,16 +1,15 @@
 library(taskscheduleR)
 library(miniUI)
-install.packages("cronR")
 library(cronR)
 
-getwd()
 
 ##mac
 cmd = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/CovidAPI.R")
 cron_add(cmd, "daily", at="5PM")
 
 cmd1 = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/MovieAPI.R")
-cron_add(cmd1, "daily", at="4PM")
+cron_add(cmd1, "daily", at="6AM")
+cron_clear(ask=TRUE)
 
 cmd2 = cron_rscript("/Users/Bailey/Desktop/Fall 2023/StatConsultation/imbddata.R")
 cron_add(cmd1, "daily", at="3PM")
